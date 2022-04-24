5e0cb7a4f5d46d286145bab67d1a083a3694a428
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-23 19:45:54 -0500

    Paso 14: Agregando rules básicas para ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
         "browser": true,
         "commonjs": true,
         "es2021": true,
-      "jest": true
+        "jest": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
     }
-}
+};
