a929769b8591c4598f16e107d9f5947872757933
Carlo Gilmar <carlogilmar12@gmail.com>
2022-04-23 19:47:57 -0500

    Paso 15: Ejecutando linter para refactorizar código mal escrito

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,10 @@
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
-	test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
-		const explorers = [{mission: "node"}]
-		const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-		expect(explorersInNode.length).toBe(1)
-	})
+    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    });
 
-})
+});
