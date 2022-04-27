687c0a450c061554aae62b93cf12bb700770ea65
LJosueCM <ljosuecm@gmail.com>
2022-04-27 18:41:57 -0500

    1) Añadida prueba a requerimiento

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,18 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 2: Endpoint explorers con stack x", () => {
+        const name = "javascript";
+        const explorers = Reader.readJsonFile("explorers.json")
+        const explorersWithStack = ExplorerService.filterByStack(explorers, name);
+        expect(explorersWithStack.lenght).toBe(11);
     });
 
 });
