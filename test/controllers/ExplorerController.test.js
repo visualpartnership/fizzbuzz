d69b50040c3466139aca359a0cc954e27838cb71
LJosueCM <ljosuecm@gmail.com>
2022-04-27 18:53:10 -0500

    3) Prueba para requerimiento en ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,9 @@
+const ExplorerService = require("./../../lib/controllers/ExplorerController");
+
+describe("Tests para ExplorerController", () => {
+    test("Requerimiento 1: Obtener explorers por stack", () => {
+        const name = "javascript"
+        const explorersWithStack = ExplorerService.getExplorersWithStack(name);
+        expect(explorersWithStack.length).toBe(11);
+    });
+});
