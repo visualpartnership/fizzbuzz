3821cd11beb6adc84714dc44a4ac01ba80180fb9
hectorSampieri <hoscmx@outlook.com>
2022-04-27 13:48:28 -0500

    Se agrega la funcion filterByStack en la clase Explorer Service con su respectiva prueba unitaria

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,12 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+    test("Requerimiento 4: Regresar toda la lista de explorers filtrados por un stack.", () => {
+        const explorers = [{name: "Woopa1",githubUsername: "ajolonauta1",score: 1,mission: "node",stacks: ["java","reasonML", "elm"]}];
+        const explorersInStack = ExplorerService.filterByStack(explorers, "java");
+        console.log(explorersInStack)
+        expect(explorersInStack.length).toBe(1);
+    });
 
 });
+
