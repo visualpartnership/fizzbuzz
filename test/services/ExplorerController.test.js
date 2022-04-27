f3093e02425897ffafe4b34ab98609950e30d8bd
AndreaCuriel <acurielipn@hotmail.com>
2022-04-27 17:58:55 -0500

    9) Ejección de Linter

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -1,9 +1,9 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("ExplorerController test", () => {
-  test("Test 1, Filter for stack", () => {
-    const stackList =
+    test("Test 1, Filter for stack", () => {
+        const stackList =
       ExplorerController.getExplorersUsernamesByStack("javascript");
-    expect(stackList[0].name).toBe("Woopa1");
-  });
+        expect(stackList[0].name).toBe("Woopa1");
+    });
 });
