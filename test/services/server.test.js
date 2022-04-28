6a1fcdcdbb9c9e4bcb2aee6ec27bec5b23114443
Ingrzs <ing.ramirozs@gmail.com>
2022-04-28 18:53:14 -0500

    prueba actualizada

--- test/services/server.test.js
+++ test/services/server.test.js
@@ -5,7 +5,7 @@ describe("Tests para Explorer service del metodo que se pidio  bystack", () => {
         const explorers = Reader.readJsonFile("explorers.json");
         const explorerstack = ExplorerService.getexplorerstack(explorers,"javascript");
     
-        expect(explorerstack[0].name).toBe("Wopa1");
+        expect(explorerstack[0].name).toBe("Woopa1");
     });
 
 });
