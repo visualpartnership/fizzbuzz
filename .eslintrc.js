dc9cb82e07d8148148b299b77bfc35eb78e39ae8
MarioMog <mamg3108@gmail.com>
2022-04-28 12:27:40 -0500

    chore: linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,18 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es2021": true
+        "es2021": true,
+        "jest":true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
-        "extends": "airbnb"
+        "no-unused-vars": "off",
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
     }
 }
