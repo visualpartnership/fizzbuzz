f0abc7fcd4833577b41d76a3fc6fe13ee89ed7c7
Ingrzs <ing.ramirozs@gmail.com>
2022-04-28 18:48:50 -0500

    prueba de test para explorerservice by stack

--- test/services/server.test.js
+++ test/services/server.test.js
@@ -1,11 +1,11 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 const Reader = require("./../../lib/utils/reader")
-describe("Tests para Endpoint de bystack", () => {
+describe("Tests para Explorer service del metodo que se pidio  bystack", () => {
     test("Requerimiento obtener stack de usuarios", () => {
         const explorers = Reader.readJsonFile("explorers.json");
         const explorerstack = ExplorerService.getexplorerstack(explorers,"javascript");
     
-        expect(explorerstack[0].name).toBe("Woopa1");
+        expect(explorerstack[0].name).toBe("Wopa1");
     });
 
 });
