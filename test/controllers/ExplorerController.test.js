28f6763e0b665e8042ee1bce355915108e478b3b
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-28 16:07:18 -0500

    Agregando método getExplorersByStack() en ExplorerController con su respectiva prueba de unidad

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,19 @@
+const Reader = require("../../lib/utils/reader");
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+
+describe("Unit Test for Explorer Controller", () => {
+    
+    test("Get explorers by stack", () => {
+        const explorers = Reader.readJsonFile('explorers.json')
+        const explorersByStackT = [];
+        explorers.forEach(element => {
+           if (element.stacks.includes('javascript')){
+               explorersByStackT.push(element)
+           } 
+        });
+        const explorersByStack = ExplorerController.getExplorersByStack(
+            "javascript",
+        );
+        expect(explorersByStack).toStrictEqual(explorersByStackT);
+    });
+});
