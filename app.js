a317accbe9a659ead96831d2965fd1c7b6f600e1
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-04-28 16:23:59 -0500

    Agregando endpoint a server para filtrar lista de explorers por stack

--- app.js
+++
@@ -1,6 +0,0 @@
-const ExplorerService = require("./lib/services/ExplorerService");
-const Reader = require("./lib/utils/reader")
-
-const explorers = Reader.readJsonFile("explorers.json")
-explorersbystack = ExplorerService.filterByStack(explorers,"javascript");
-console.log(explorersbystack)
