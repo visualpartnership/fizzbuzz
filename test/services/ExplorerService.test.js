8dcfdb87e8911a85dfc698ecdf7705649de8c8c6
Raul Quintero <raulquintero13@gmail.com>
2022-04-28 00:35:20 -0700

    adding new feature in ExplorerService & test succesful

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/Reader.js");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,9 +7,10 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-    
+
     test("Requerimiento 4: Crear un nuevo endpoint para un cliente nuevo getExplorerByStack",() =>{
         const stack = "javascript";
+        const explorers = Reader.readJsonFile("explorers.json");
         const explorersByStack = ExplorerService.filterByStack(explorers, stack);
         console.log(explorersByStack);
         expect(explorersByStack.length).toBe(11)
