d1b5dbe38a7e4d1f8677c539de3f063c79f4e80c
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-28 13:22:27 -0500

    Corriendo lint

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -9,7 +9,7 @@ describe("Tests para ExplorerService", () => {
 
     test("Requerimiento 2: Regresar toda la lista de explorers filtrados por un stack.", () => {
         const explorers = [{name: "explorer1", mission: "node", stacks: ["javascript"]}, 
-        {name: "explorer2", mission: "node", stacks: ["javascript", "groovy"]}];
+            {name: "explorer2", mission: "node", stacks: ["javascript", "groovy"]}];
 
         const explorersInJavascript = ExplorerService.filterByStack(explorers, "javascript");
         expect(explorersInJavascript.length).toBe(2);
