5ff56af5e2c39dba418190eaf1f5270aa00e34b2
Jorge Salmerón <jsalmeron18@outlook.com>
2022-04-28 15:46:45 -0500

    Se agrega node_modules, y métodos para satisfacer requerimiento de filtrar por stack

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -4,28 +4,28 @@ describe("Unit Test for ExplorerController class", () => {
 	
 	test("1) Test of filterByMission method", () => {
 		const mission = "java";
-		const result = ExplorerController.getExplorerByMission(mission)
-		expect(result[0]).toBe("Woopa6")
+		const result = ExplorerController.getExplorersByMission(mission)
+		expect(result[0].name).toBe("Woopa6")
 	});
 	test("2) Test of applyFizzbuzz method", ()=>{
 		const score = 15;
 		const result = ExplorerController.applyFizzbuzz(score)
-		expect(result).toBe("FIZZBUZZ")
+		expect(result).toEqual("FIZZBUZZ")
 	});
 	test("3) Test of usernameByMission Method", ()=>{
 		const mission = "java"
-		const result = ExplorerController.getExplorersUsernameByMission(mission)
+		const result = ExplorerController.getExplorersUsernamesByMission(mission)
 		expect(result[0]).toBe("ajolonauta6")
 	});
 	test("4) Test of amountByMission Method" ,() =>{
 		const mission = "java"
-		const result = ExplorerController.getExplorersAmountByMission(mission)
-		expect(10).toBe(5)
+		const result = ExplorerController.getExplorersAmonutByMission(mission)
+		expect(result).toBe(5)
 	});
 
 	test("5) Test of filteByStack method", () => {
 		const stack = "reasonML"
-		const result = Explorer.Controller.getExplorerByStack(stack)
+		const result = ExplorerController.getExplorersByStack(stack)
 		expect(result.lenght).toBe(9)
 	});
 
