40bf341c5fac0929b3cf1bec159e0c33779abc35
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-28 14:22:28 -0500

    1.- Se agregaron los metodos para el requerimiento en ExplorerService y ControllerService y sus pruebas correspondientes

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader")
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,5 +7,15 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+
+    test("Tets de Requerimiento 4 open source, obtener lista de explorers por stack", () => {
+
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersStack = ExplorerService.getExplorersListByStack(explorers,"javascript");
+        expect(explorersStack[0].name).toBe("Woopa1");
+        expect(explorersStack[1].name).toBe("Woopa2");
+        expect(explorersStack[2].name).toBe("Woopa4");
+    });
+
 
 });
