6290a881b45fc720d58dd50d4c34038c03e65fa6
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-28 16:10:53 -0500

    3.- Se corrio el linter y linter-fix

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,5 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
-const Reader = require("./../../lib/utils/reader")
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
