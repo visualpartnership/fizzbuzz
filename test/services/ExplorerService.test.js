f4da796a58268b5e5507124196dfa22ac4ab55c4
joslo2345 <soulsilversx@gmail.com>
2022-04-29 02:18:27 -0500

    paso 23: agregar prueba unitaria al metodo filterByStack  a /test/services/ExplorerService.test.js

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,17 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-
+    
+    test("Requerimiento 4: obtener todo los explorers en un stack", () => {
+        const explorers = [
+        {"nombre":'woopa1',"stack":'java'},
+        {"nombre":'woopa2',"stack":'javascript'},
+        {"nombre":'woopa3',"stack":'java'},
+        {"nombre":'woopa4',"stack":'javascript'},
+        {"nombre":'woopa5',"stack":'java'},
+        {"nombre":'woopa6',"stack":'elixir'}
+        ];
+        const explorersWithStackJava = ExplorerService.filterByStack(explorers,'java');
+        expect(explorersWithStackJava[0].stack).toBe('java');
+    });
 });
