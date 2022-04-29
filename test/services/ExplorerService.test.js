763305a300a7687a0d56492099a1679ca34c3a66
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-29 18:15:38 -0500

    Limpieza de código deacuerdo a las reglas de Eslint

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,5 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
-const Reader = require("./../../lib/utils/reader")
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -8,8 +8,8 @@ describe("Tests para ExplorerService", () => {
         expect(explorersInNode.length).toBe(1);
     });
     test("Requerimiento 2: Obtener explorers filtrados por un stack", () => {
-        const explorers = Reader.readJsonFile('test/explorers.test.json')
+        const explorers = Reader.readJsonFile("test/explorers.test.json");
         const explorersWithStack = ExplorerService.getExplorerByStack(explorers, "elixir");
         expect(explorersWithStack.length).not.toBe(0);
-    })
+    });
 });
