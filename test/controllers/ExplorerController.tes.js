f3764b2ced6c4302efb0b214ad767baa999714ec
Alexis Guzman <alexis12989x@gmail.com>
2022-04-29 13:05:42 -0500

    test(jest): create tests for explorer controller class

---
+++ test/controllers/ExplorerController.tes.js
@@ -0,0 +1,13 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
+
+describe("Test for ExplorerController", () => {
+    test("1. Filter list of explorers by mission", () => {
+        const explorers = ExplorerController.getExplorersByMission("node");
+        expect(explorers.length).toBe(10);
+    });
+
+    test("2. Filter list of explorers by stack", () => {
+        const explorers = ExplorerController.getExplorersByStack("javascript");
+        expect(explorers.length).toBe(11);
+    });
+});
