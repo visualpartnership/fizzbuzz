c92c26bada07f0aec6d5f4f9c0858cd2f082a282
Alexis Guzman <alexis12989x@gmail.com>
2022-04-29 12:58:01 -0500

    test(jest): add test for filter by stack function

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,21 @@
+const Reader = require("./../../lib/utils/reader");
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
-        const explorers = [{mission: "node"}];
+        const explorers = [{ mission: "node" }];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
 
+    test("Requerimiento 2: Filtrar explorers por stack", () => {
+    // const explorers = { stacks: ["c#", "javascript", "elixir"] };
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNode = ExplorerService.filterByStack(
+            explorers,
+            "javascript"
+        );
+
+        expect(explorersInNode[0].stacks[0]).toBe("javascript");
+    });
 });
