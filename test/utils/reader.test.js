165cd699da46f50ed2b35a02abfc83ca8ecf166d
Ingrzs <ing.ramirozs@gmail.com>
2022-04-29 00:07:33 -0500

    test para reader.js

---
+++ test/utils/reader.test.js
@@ -0,0 +1,10 @@
+const Reader = require("./../../lib/utils/reader");
+
+
+describe("Tests Para Reader", () => {
+
+    test("Requerimiento Test para Json", () => {
+        const explorersInNode = Reader.readJsonFile("explorers.json");
+        expect(explorersInNode.length).toBe(15);
+    });
+})
