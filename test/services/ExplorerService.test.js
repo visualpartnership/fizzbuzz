14b673ef912a4b8dbf1657cf1cbfe4749d2e4ac5
SaraiMV <sarizvargas@gmail.com>
2022-04-28 20:10:16 -0500

    Adding unit test for filterByStack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,33 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
-    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
-        const explorers = [{mission: "node"}];
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(1);
-    });
+  test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+    const explorers = [{ mission: "node" }];
+    const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+    expect(explorersInNode.length).toBe(1);
+  });
 
+  test("Requerimiento 2: Filtrar explorers por stack", () => {
+    const explorers = [
+      {
+        name: "Woopa1",
+        githubUsername: "ajolonauta1",
+        score: 1,
+        mission: "node",
+        stacks: ["javascript", "reasonML", "elm"],
+      },
+      {
+        name: "Woopa3",
+        githubUsername: "ajolonauta3",
+        score: 3,
+        mission: "node",
+        stacks: ["elixir", "groovy", "reasonML"],
+      },
+    ];
+    const explorersFilterByStack = ExplorerService.filterByStack(
+      explorers,
+      "javascript"
+    );
+    expect(explorersFilterByStack.length).toBeGreaterThanOrEqual(0);
+  });
 });
