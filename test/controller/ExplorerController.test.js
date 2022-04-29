1eb303514fa3c8a780f09864e1871bd3b506863a
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-29 13:25:57 -0500

    test: getExplorerByStack suit test added

---
+++ test/controller/ExplorerController.test.js
@@ -0,0 +1,12 @@
+const ExplorerController = require('./../../lib/controllers/ExplorerController');
+
+describe("Test para ExplorerController", () => {
+    test("getExplorerByStack function returns a list of explorers that contain into its property stacks the stack value passed", () => {
+        //arrange and acts
+        const explorersJS = ExplorerController.getExplorerByStack('javascript');
+        //asserts
+        for(let i = 0; i < explorersJS.length; i++){
+            expect(explorersJS[i].stacks.includes('javascript')).toBeTruthy()
+        }
+    });
+});
