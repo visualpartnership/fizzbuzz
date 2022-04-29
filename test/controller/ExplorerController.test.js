389a536d97003e574d0dabfe3fdac1a6a7a08543
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-29 13:42:58 -0500

    fix: errors using ESlint -fix

--- test/controller/ExplorerController.test.js
+++ test/controller/ExplorerController.test.js
@@ -1,12 +1,12 @@
-const ExplorerController = require('./../../lib/controllers/ExplorerController');
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Test para ExplorerController", () => {
     test("getExplorerByStack function returns a list of explorers that contain into its property stacks the stack value passed", () => {
         //arrange and acts
-        const explorersJS = ExplorerController.getExplorerByStack('javascript');
+        const explorersJS = ExplorerController.getExplorerByStack("javascript");
         //asserts
         for(let i = 0; i < explorersJS.length; i++){
-            expect(explorersJS[i].stacks.includes('javascript')).toBeTruthy()
+            expect(explorersJS[i].stacks.includes("javascript")).toBeTruthy();
         }
     });
 });
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,5 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
-const Reader = require('./../../lib/utils/reader');
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -9,11 +9,11 @@ describe("Tests para ExplorerService", () => {
     });
     test("Requeriemto 2: filtrar los explorers por stack", () => {
         //arrange and acts
-        const explorers = Reader.readJsonFile('explorers.json');
-        const explorersJS = ExplorerService.getExplorersByStack(explorers, 'JavaScript')
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersJS = ExplorerService.getExplorersByStack(explorers, "JavaScript");
         //asserts
         for(let i = 0; i < explorersJS.length; i++){
-            expect(explorersJS[i].stacks.includes('javascript')).toBeTruthy()
+            expect(explorersJS[i].stacks.includes("javascript")).toBeTruthy();
         }
     });
 
