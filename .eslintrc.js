c57600da4347423754723ede6ac5d6bea484bc82
Joel Hoil <yoelvann@gmail.com>
2022-04-29 00:58:51 -0500

    Adding eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,13 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es2021": true,
-        "jest": true
+        "es2021": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
-        "no-unused-vars": "off",
         indent: ["error", 4],
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double"],
