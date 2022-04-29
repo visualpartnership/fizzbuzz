1fbbe92b4a759bac9ba103764129483b4de85737
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-29 16:25:18 -0500

    Agregando prueba funcional ExplorerController

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -3,6 +3,6 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 describe("Tests para ExplorerController", () => {
     test("Requerimiento : Verificar que se obtienen los explorers con stack especifico", () => {
             const explorersInStack = ExplorerController.getExplorersByStack("javascript");
-            expect(explorersInStack).toBe(1);;
+            expect(explorersInStack).not.toBeUndefined();;
     });
 });
