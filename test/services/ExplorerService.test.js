c902baa97a1209cd8dbd8475515ae6f3c308d4dd
Ingrzs <ing.ramirozs@gmail.com>
2022-04-28 23:20:33 -0500

    Requerimientos completos para ExplorerService.js

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,32 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader")
 
 describe("Tests para ExplorerService", () => {
+
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+
+    
+     test("Requerimiento 2 cantida de explorer",() =>{
+         const explorers = [{mission: "node"}, {mission:"node"}]
+         const explorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+         expect(explorersByMission).toBe(2)
+     })
+     
+
+     test("Requerimiento 3 por Usernames",()=>{
+        const explorer = [{ mission: "node", githubUsername: "Ramiro" },{ mission: "node", githubUsername: "rzs"}];
+        const ExplorerAmount = ExplorerService.getExplorersUsernamesByMission(explorer,"node");  
+        expect(ExplorerAmount[0]).toBe("Ramiro")
+     })
+
+     test ("Requerimiento 4 por stack",()=>{
+         const explorers = Reader.readJsonFile("explorers.json")
+        const explorerstack = ExplorerService.getexplorerstack(explorers, "javascript");
+        expect(explorerstack[0].name).toBe("Woopa1");
+     })
 
 });
