ff8398a34c94a30a42490a357f22c1c3b77be916
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-29 16:36:30 -0500

    Agregando ESLinter y fixeando

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -2,7 +2,7 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Tests para ExplorerController", () => {
     test("Requerimiento : Verificar que se obtienen los explorers con stack especifico", () => {
-            const explorersInStack = ExplorerController.getExplorersByStack("javascript");
-            expect(explorersInStack).not.toBeUndefined();;
+        const explorersInStack = ExplorerController.getExplorersByStack("javascript");
+        expect(explorersInStack).not.toBeUndefined();
     });
 });
