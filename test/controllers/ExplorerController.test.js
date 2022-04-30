9df1b61231b9c960eb528d2c4e550b764ce6bcce
Julieta Delgado <julidmeza@gmail.com>
2022-04-30 15:45:52 -0500

    Run linter

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,11 +2,11 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Tests para ExplorerController", () => {
     test("Requerimiento 2: Explorers que tengan en stack el valor recibido", () => {
-        const stack = "javascript"
+        const stack = "javascript";
         const explorersByStack = ExplorerController.getExplorersByStack(stack);
         explorersByStack.forEach((explorer) => {
             expect(explorer.stacks).toEqual(
-                expect.arrayContaining([stack]))
+                expect.arrayContaining([stack]));
         });
         expect(explorersByStack.length).toBe(11);
     });
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -9,11 +9,11 @@ describe("Tests para ExplorerService", () => {
 
     test("Requerimiento 2: Explorers que tengan en stack el valor recibido", () => {
         const explorers = [ { "name": "Woopa1", "githubUsername": "ajolonauta1", "score": 1, "mission": "node", "stacks": [ "javascript", "reasonML", "elm" ] }, { "name": "Woopa2", "githubUsername": "ajolonauta2", "score": 2, "mission": "node", "stacks": [ "javascript", "groovy", "elm" ] }, { "name": "Woopa3", "githubUsername": "ajolonauta3", "score": 3, "mission": "node", "stacks": [ "elixir", "groovy", "reasonML" ] }, { "name": "Woopa4", "githubUsername": "ajolonauta4", "mission": "node", "score": 4, "stacks": [ "javascript" ] }];
-        const stack = "javascript"
+        const stack = "javascript";
         const explorersByStack = ExplorerService.filterByStack(explorers, stack);
         explorersByStack.forEach((explorer) => {
             expect(explorer.stacks).toEqual(
-                expect.arrayContaining([stack]))
+                expect.arrayContaining([stack]));
         });
         expect(explorersByStack[0].name).toBe("Woopa1");
         expect(explorersByStack[2].name).toBe("Woopa4");
