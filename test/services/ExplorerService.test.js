94505b458447e714c2ff855340b131d4ac8013d8
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-30 00:47:42 -0500

    linter fix sugestion

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -10,7 +10,6 @@ describe('Tests para ExplorerService', () => {
   test('Requerimiento 2: Devolver lista de explorers que cumplen con stack', () => {
     const explorers = [{ name: 'explorer1', stacks: ['node', 'javascript'] }, { name: 'explorer2', stacks: ['javascript'] }, { name: 'explorer3', stacks: ['node'] }];
     const explorersWithStack = ExplorerService.getExplorersByStack(explorers, 'javascript');
-    console.log(explorersWithStack);
     expect(explorersWithStack.length).toBe(2);
   });
 });
