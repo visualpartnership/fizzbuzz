8bc6fe8a4ec8b655f0f4a3ecd24f779a9bd0c7b6
Julieta Delgado <julidmeza@gmail.com>
2022-04-30 15:09:07 -0500

    Correction in test

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -8,12 +8,14 @@ describe("Tests para ExplorerService", () => {
     });
 
     test("Requerimiento 2: Explorers que tengan en stack el valor recibido", () => {
-        const explorers = [ { "name": "Woopa1", "githubUsername": "ajolonauta1", "score": 1, "mission": "node", "stacks": [ "javascript", "reasonML", "elm" ] }, { "name": "Woopa2", "githubUsername": "ajolonauta2", "score": 2, "mission": "node", "stacks": [ "javascript", "groovy", "elm" ] }, { "name": "Woopa3", "githubUsername": "ajolonauta3", "score": 3, "mission": "node", "stacks": [ "elixir", "groovy", "reasonML" ] }, { "name": "Woopa4", "githubUsername": "ajolonauta4", "mission": "node", "score": 4, "stacks": [ "node" ] }];
-        const stack = ["javascript"]
+        const explorers = [ { "name": "Woopa1", "githubUsername": "ajolonauta1", "score": 1, "mission": "node", "stacks": [ "javascript", "reasonML", "elm" ] }, { "name": "Woopa2", "githubUsername": "ajolonauta2", "score": 2, "mission": "node", "stacks": [ "javascript", "groovy", "elm" ] }, { "name": "Woopa3", "githubUsername": "ajolonauta3", "score": 3, "mission": "node", "stacks": [ "elixir", "groovy", "reasonML" ] }, { "name": "Woopa4s", "githubUsername": "ajolonauta4", "mission": "node", "score": 4, "stacks": [ "javascript" ] }];
+        const stack = "javascript"
         const explorersByStack = ExplorerService.filterByStack(explorers, stack);
         explorersByStack.forEach((explorer) => {
-            expect(explorer.stacks).expect.arrayContaining(stack);
+            expect(explorer.stacks).toEqual(
+                expect.arrayContaining([stack]))
         });
+        expect(explorersByStack[2].name).toBe("Woopa4");
     });
 
 });
