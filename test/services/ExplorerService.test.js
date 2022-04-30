f5777fc0701acb2e01fa047b7418b7c146edd1fd
Luis Armando Prado <armap1999@hotmail.com>
2022-04-30 10:14:08 -0500

    Se aplico lintern y se creo getExplorersByStack con su respectiva prueba.

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,15 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    }); 
+    test("Test para stack", () => {
+        const explorers = Reader.readJsonFile("./explorers.json");
+        expect(ExplorerService.getExplorersByStack(explorers,"javascript").length).toBe(11);
     });
 
 });
