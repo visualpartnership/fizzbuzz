b3209865330f386de5e2b234e69c1579153d8bff
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-29 21:42:33 -0500

    Added test for filterByStack method

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,9 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-
+    test("Requerimiento 2: Obtener todos los explorers con cierto stack", () => {
+        const explorers = [{name: "explorer1", stack: "java"}, {name: "explorer2", stack:"javascript"}, {name: "explorer3", stack: "java"}];
+        const explorersInJava = ExplorerService.filterByStack(explorers, "java");
+        expect(explorersInJava).toEqual([{name: "explorer1", stack:"java"}, {name: "explorer3", stack: "java"}]);
+    });
 });
