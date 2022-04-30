c960cf019ec22ffb507d9febd701e280acd1bfa7
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-04-29 22:39:31 -0500

    Solucion a la parte 4 de Fizzbuzz resolver codigo tronado Reader

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("./../../lib/utils/Reader");
 
 describe("Pruebas unitarias para el Reader", () => {
     test("Requerimiento 1: Contar los datos de muestra", () => {
-        const explorers = Reader.readJsonFile("./test/data/explorers-test.json");
+        const explorers = Reader.readJsonFile("./test/data/explorers.json");
         expect(explorers.length).toBe(15);
     });
 });
