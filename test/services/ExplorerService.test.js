fcc2c1eadabe1d56eed2bbe08ac52fd395b6b409
aceron <ariceg@comunidad.unam.mx>
2022-04-30 14:44:55 -0500

    Test pass

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,4 @@
-const ExplorerService = require("./../../lib/services/ExplorerService");
+const ExplorerService = require("../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,5 +6,14 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-
+    test("Requerimiento 2: Obtener explorers en stack", () => {
+        const explorers= [{stack: ["node","react"]},{stack: ["node","react","javascript"]}];
+        const explorersInNode = ExplorerService.filterByStack(explorers, "node");
+        expect(explorersInNode.length).toBe(2);
+    })
+    test("Requerimiento 2.1: Obtener explorers en stack", () => {
+        const explorers= [{stack: ["node","react"]},{stack: ["node","react","javascript"]}];
+        const explorersInNode = ExplorerService.filterByStack(explorers, "javascript");
+        expect(explorersInNode.length).toBe(1);
+    })
 });
