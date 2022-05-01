59e0cb690053f4058944be2e44c6fbed61099cf0
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-05-01 18:18:31 -0500

    6) Limpiando el codigo de Test

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,37 +1,37 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
-const Reader=require("./../../lib/utils/reader")
+const Reader=require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 2: Calcular todos los explorers en una misión", () => {
         const explorers=
             [
                 {
-                  "name": "Woopa1",
-                  "githubUsername": "ajolonauta1",
-                  "score": 1,
-                  "mission": "node",
-                  "stacks": "javascript"
+                    "name": "Woopa1",
+                    "githubUsername": "ajolonauta1",
+                    "score": 1,
+                    "mission": "node",
+                    "stacks": "javascript"
                 },
                 {
-                  "name": "Woopa2",
-                  "githubUsername": "ajolonauta2",
-                  "score": 2,
-                  "mission": "node",
-                  "stacks": [
-                    "groovy",
-                    "elm"
-                  ]
+                    "name": "Woopa2",
+                    "githubUsername": "ajolonauta2",
+                    "score": 2,
+                    "mission": "node",
+                    "stacks": [
+                        "groovy",
+                        "elm"
+                    ]
                 },
             ];
 
         const result=[{
-          "name": "Woopa1",
-          "githubUsername": "ajolonauta1",
-          "score": 1,
-          "mission": "node",
-          "stacks": "javascript"
+            "name": "Woopa1",
+            "githubUsername": "ajolonauta1",
+            "score": 1,
+            "mission": "node",
+            "stacks": "javascript"
         }];
-        const explorer=ExplorerService.getExplorersStackByMission(explorers,"javascript")
+        const explorer=ExplorerService.getExplorersStackByMission(explorers,"javascript");
         expect(explorer).toStrictEqual(result);
     });
 
