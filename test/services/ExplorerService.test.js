cc1961b6f25dcf0a49e6ba3cb4d69bae233bf5f8
Jose Luis <launchx06519@innovaccion.mx>
2022-05-01 17:35:01 -0500

    6.Haciendo prueba unitaria

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,5 +7,8 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-
+    test("Requerimiento 2: Nombres de los explorers con stack solicidado", () => {
+        const explorerswithStack= ExplorerController.getExplorerStack("javascript");
+        expect(explorerswithStack).toMatchObject(["ajolonauta1", "ajolonauta2", "ajolonauta4", "ajolonauta5", "ajolonauta9", "ajolonauta10", "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"]);
+    });
 });
