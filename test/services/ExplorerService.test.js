1dc214a9e314f576e35de20a1fe3731b7913e39c
joseduardo <reedu_@outlook.com>
2022-05-01 03:20:07 -0500

    Se agrega el metodo requerido en ExplorerService asi como su prueba de unidad

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,5 +7,11 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+
+    test("Prueba de unidad para el metodo de filtrado por stack",()=>{
+        const explorers = Reader.readJsonFile("explorers.json");
+        explorersInStack = ExplorerService.getExplorersByStack(explorers,"javascript");
+        expect(explorersInNode.length).toBe(11);
+    })
 
 });
