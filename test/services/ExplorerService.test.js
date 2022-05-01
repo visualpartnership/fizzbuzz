712659497b2c51faa0da1e84de1c2ff77cdc5a19
MarcosLopez <marcoslm@ciencias.unam.mx>
2022-04-30 23:00:52 -0500

    Adding unit tests to ExplorerService module

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,4 +7,35 @@ describe("Tests para ExplorerService", () => {
         expect(explorersInNode.length).toBe(1);
     });
 
+    test("Requerimiento 4: Filtrando por stack dado un stack vacío", () => {
+        const explorers = require("./explorers_prueba.json");
+        const explorersByStack1 = ExplorerService.filterExplorersByStack(explorers, "");
+        const explorersByStack2 = ExplorerService.filterExplorersByStack(explorers, [""]);
+        const explorersByStack3 = ExplorerService.filterExplorersByStack(explorers, []);
+
+        expect(explorersByStack1).toEqual([]);
+        expect(explorersByStack2).toEqual([]);
+        expect(explorersByStack3).toEqual([]);
+    });
+
+    test("Requerimiento 4: Filtrando por stack dado un stack no contenido", () => {
+        const explorers = require("./explorers_prueba.json");
+        const explorersByStack = ExplorerService.filterExplorersByStack(explorers, "django");
+
+        expect(explorersByStack).toEqual([]);
+    });
+
+    test("Requerimiento 4: Filtrando por stack sin stack recibido", () => {
+        const explorers = require("./explorers_prueba.json");
+        const explorersByStack = ExplorerService.filterExplorersByStack(explorers, );
+
+        expect(explorersByStack).toEqual([]);
+    });
+
+    test("Requerimiento 4: Filtrando por stack", () => {
+        const explorers = require("./explorers_prueba.json");
+        const explorersByStack = ExplorerService.filterExplorersByStack(explorers, "javascript");
+
+        expect(explorersByStack.length).toBe(6);
+    });
 });
