80f0982ae2fdef7e66407f65a976dd81dd679e21
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-05-02 15:37:03 -0500

    prueba explorer controler passed

--- test/services/ExplorerController.test.js
+++ test/services/ExplorerController.test.js
@@ -2,7 +2,7 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Tests unitarios para FizzBuzz stacks", () => {
     test("Explorers Filtrados Por stack", () => {
-        const explorersInNode = ExplorerController.getExplorersByStack("node");
-        expect(explorersInNode.length).toBe(1);
+        const explorersInNode = ExplorerController.getExplorersByStack("javascript");
+        expect(explorersInNode.length).toBe(11);
     });
 });
