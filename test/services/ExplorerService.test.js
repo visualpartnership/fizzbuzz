4f6a989422a0bdd567f1d524c696c78fa6454a46
Jorge Aguilar <jorge.agvel@gmail.com>
2022-05-02 02:42:31 -0500

    Create test for obtain by stack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,9 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-
+    test("Requerimiento 2: Lista explorers por stack", () => {
+        const explorers = [{mission: "node", stack: "javascript"}, {mission: "node", stack: "frontend"}];
+        const explorersContainsJavaScript = explorers.filter(explorer => explorer.stack === "javascript");
+        expect(explorersContainsJavaScript.length).toBe(1);
+    });
 });
