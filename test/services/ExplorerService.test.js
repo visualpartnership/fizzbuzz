436d93bcb87102bb7439b7bbd17fd4bb145b43fa
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-05-02 15:46:43 -0500

    Add endpoint by stack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,4 +7,32 @@ describe("Tests para ExplorerService", () => {
         expect(explorersInNode.length).toBe(1);
     });
 
+    test("Requerimiento 2: Regresar todos los explorers en una stack", () => {
+        const explorers = [{
+            "name": "Woopa1",
+            "githubUsername": "ajolonauta1",
+            "score": 1,
+            "mission": "node",
+            "stacks": [
+                "javascript",
+                "reasonML",
+                "elm"
+            ]
+        },
+        {
+            "name": "Woopa3",
+            "githubUsername": "ajolonauta3",
+            "score": 3,
+            "mission": "node",
+            "stacks": [
+                "elixir",
+                "groovy",
+                "reasonML"
+            ]
+        }
+        ];
+        const explorersInStack = ExplorerService.getExplorersByStack(explorers, "javascript");
+        expect(explorersInStack.length).toBe(1);
+    });
+
 });
