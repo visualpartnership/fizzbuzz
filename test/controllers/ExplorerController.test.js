781218c1a4202485d163d9f4fc5e8e0586be5320
joslo2345 <soulsilversx@gmail.com>
2022-05-03 03:23:03 -0500

    paso 31:agregando el codigo correspondiente a lib/controllers/ExplorerController.js y a /lib/bot.js

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -9,19 +9,19 @@ describe("Tests para ExplorerController", () => {
     test("regresar una respuesta para numero en fizzbuzzbot telegram", () => {
         const number1 = 3;
         const fizzbuzzBotResponse1 = ExplorerController.applyFizzbuzzBotResponse(number1);
-        expect(fizzbuzzBotResponse1).toBe("FIZZ");
+        expect(fizzbuzzBotResponse1).toBe(`Tu número es: ${number1}. Validación: ${ExplorerController.applyFizzbuzz(number1)}`);
 
         const number2 = 5;
         const fizzbuzzBotResponse2 = ExplorerController.applyFizzbuzzBotResponse(number2);
-        expect(fizzbuzzBotResponse2).toBe("BUZZ");
+        expect(fizzbuzzBotResponse2).toBe(`Tu número es: ${number2}. Validación: ${ExplorerController.applyFizzbuzz(number2)}`);
 
         const number3 = 15;
         const fizzbuzzBotResponse3 = ExplorerController.applyFizzbuzzBotResponse(number3);
-        expect(fizzbuzzBotResponse3).toBe("FIZZBUZZ");
+        expect(fizzbuzzBotResponse3).toBe(`Tu número es: ${number3}. Validación: ${ExplorerController.applyFizzbuzz(number3)}`);
 
         const number4 = 7;
         const fizzbuzzBotResponse4 = ExplorerController.applyFizzbuzzBotResponse(number4);
-        expect(fizzbuzzBotResponse4).toBe(number4);
+        expect(fizzbuzzBotResponse4).toBe(`Tu número es: ${number4}. Validación: ${ExplorerController.applyFizzbuzz(number4)}`);
     });
 
 });
