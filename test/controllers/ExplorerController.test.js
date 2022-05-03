4904f75f284f5dad7e31adcea84154361ec3b8c6
joslo2345 <soulsilversx@gmail.com>
2022-05-03 03:13:38 -0500

    paso 30: agregar pruebas unitarias para el nuevo metodo estatico de /lib/controllers/ExplorerController.js , metodo estatico applyFizzbuzzBotResponse

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -5,4 +5,23 @@ describe("Tests para ExplorerController", () => {
         const explorersWithStackJavascript = ExplorerController.getExplorersByStack("javascript");
         expect(explorersWithStackJavascript[0].stacks[0]).toBe("javascript");
     });
+
+    test("regresar una respuesta para numero en fizzbuzzbot telegram", () => {
+        const number1 = 3;
+        const fizzbuzzBotResponse1 = ExplorerController.applyFizzbuzzBotResponse(number1);
+        expect(fizzbuzzBotResponse1).toBe("FIZZ");
+
+        const number2 = 5;
+        const fizzbuzzBotResponse2 = ExplorerController.applyFizzbuzzBotResponse(number2);
+        expect(fizzbuzzBotResponse2).toBe("BUZZ");
+
+        const number3 = 15;
+        const fizzbuzzBotResponse3 = ExplorerController.applyFizzbuzzBotResponse(number3);
+        expect(fizzbuzzBotResponse3).toBe("FIZZBUZZ");
+
+        const number4 = 7;
+        const fizzbuzzBotResponse4 = ExplorerController.applyFizzbuzzBotResponse(number4);
+        expect(fizzbuzzBotResponse4).toBe(number4);
+    });
+
 });
