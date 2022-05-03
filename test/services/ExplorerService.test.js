dfc193b3be861b42316e6098f881ca5000f9aa46
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-05-02 23:36:56 -0500

    Test de filtrado de explorers por staks

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,20 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+    test("Nuevo requerimiento: Lista de explorers filtrados por stack", () => {
+        const explorers = [
+            {
+                name: "Woopa1",
+                stacks: ["javascript"]
+            },
+            {
+                name: "Woopa2",
+                stacks: ["java"]
+            }
+        ];
+
+        const explorersByStacks = ExplorerService.getExplorerStacks(explorers,"javascript");
+        expect(explorersByStacks[0].stacks[0]).toBe("javascript");
+    });
 
 });
