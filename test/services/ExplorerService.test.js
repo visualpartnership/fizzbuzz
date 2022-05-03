869f50efd01cb80c4088c1ff2790332ccda17f72
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-03 01:26:54 -0500

    Agrego prueba de unidad de la funcion añadida

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,7 +7,7 @@ describe("Tests para ExplorerService", () => {
         expect(explorersInNode.length).toBe(1);
     });
     test("Requerimiento 2: Filtrar explorers por stack", () => {
-        const explorers = [{stack: "javascript"}];
+        const explorers = [{stacks: "javascript"}];
         const explorersWithStack = ExplorerService.getExplorersByStack(explorers, "javascript");
         expect(explorersWithStack.length).toBe(1);
     });
