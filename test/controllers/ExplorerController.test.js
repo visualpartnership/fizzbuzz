ab3c65a7518a6336526eadd4a46c53f6ba164b04
LJosueCM <ljosuecm@gmail.com>
2022-05-03 17:59:10 -0500

    Pruebas para los dos requerimientos adicionales

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,9 +1,21 @@
-const ExplorerService = require("./../../lib/controllers/ExplorerController");
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Tests para ExplorerController", () => {
     test("Requerimiento 1: Obtener explorers por stack", () => {
         const name = "javascript";
-        const explorersWithStack = ExplorerService.getExplorersWithStack(name);
+        const explorersWithStack = ExplorerController.getExplorersWithStack(name);
         expect(explorersWithStack.length).toBe(11);
+    });
+
+    test("Requerimiento 2: Refactorizacion de bot telegram (numero fizzbuzz)", () => {
+        const numero = 15;
+        const responseFizzBuzz = ExplorerController.botFizzBuzz(numero);
+        expect(responseFizzBuzz).toBe("Tu número es: 15. Validación: FIZZBUZZ");
+    });
+
+    test("Requerimiento 3: Refactorizacion de bot telegram (nombre de explorers por mision)", () => {
+        const mision = "node";
+        const explorersByMission = ExplorerController.botExplorersByMission(mision);
+        expect(explorersByMission).toBe('Lista de explorers para la mision node: ["ajolonauta1","ajolonauta2","ajolonauta3","ajolonauta4","ajolonauta5","ajolonauta11","ajolonauta12","ajolonauta13","ajolonauta14","ajolonauta15"]');
     });
 });
