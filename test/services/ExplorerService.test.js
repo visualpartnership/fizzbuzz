46fd3de1b2f81dbde8396b1655318ae8da35081a
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-05-03 22:00:01 -0500

    Se creo la prueba para el nuevo requerimiento

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,3 +1,4 @@
+const ExplorerController = require("../../lib/controllers/ExplorerController");
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
@@ -5,6 +6,12 @@ describe("Tests para ExplorerService", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 2: Devolver todos los explorers que tengan un stack especifico.", () =>{
+        const explorers = [{name: "raul", stacks: "javascript"}, {name: "raul2", stacks: ["javascript"]}, {name: "raul3", stacks: ["javascript", "groovy", "elm"]}, {name: "raul2", stacks: ["groovy"]}];
+        const explorersByStack = ExplorerService.explorersByStack(explorers, "javascript");
+        expect(explorersByStack).toStrictEqual([{name: "raul", stacks: "javascript"}, {name: "raul2", stacks: ["javascript"]}, {name: "raul3", stacks: ["javascript", "groovy", "elm"]}]);
     });
 
 });
