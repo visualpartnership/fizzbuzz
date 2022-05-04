f37df0e99d14ef1ddbd8841a7cfaa1250073f3be
AleRamRey <launchx09432@innovaccion.mx>
2022-05-04 07:53:53 -0500

    Se agrega prueba filterByStack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,9 +1,16 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
+    
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBe(1);
+    })
+
+    test("Requerimiento 2: Filtrar explorers por stack", () => {
+        const explorers = [{stacks: "c#, javascript, elixir"}];
+        const explorersInNode = ExplorerService.filterByStack(explorers, "javascript");
         expect(explorersInNode.length).toBe(1);
     });
 
