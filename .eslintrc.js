f6001481fe1777cd3249ce12fc4137c86a89fd86
Oscar <oscarskapee@gmail.com>
2022-05-04 00:04:57 -0500

    Se restauraron archivos dañados

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,12 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-<<<<<<< HEAD
-        "es2021": true,
-        "jest": true
-=======
         "es2021": true
->>>>>>> e9e4243 (Se agrego servicio y pruebas para poder validar perfectamente el servicio)
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
-<<<<<<< HEAD
-        "no-unused-vars": "off",
-        indent: ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"]
-    }
-};
-=======
     }
 }
->>>>>>> e9e4243 (Se agrego servicio y pruebas para poder validar perfectamente el servicio)
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,16 +2,8 @@ const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
-<<<<<<< HEAD
-        const explorers = [{mission: "node"}];
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(1);
-    });
-
-=======
         // const explorers = [{mission: "node"}];
         // const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         // expect(explorersInNode.length).toBe(1);
     });
->>>>>>> e9e4243 (Se agrego servicio y pruebas para poder validar perfectamente el servicio)
 });
