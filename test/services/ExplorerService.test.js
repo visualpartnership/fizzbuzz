c411c906e1f85e18006e4dd73142285c6411de70
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-05 12:37:59 -0500

    Paso 21: Prueba para el metodo getExplorersByStack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,20 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
+    
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 2: Listar los explorers filtrados por un stack", () => {
+        
+        const explorers = Reader.readJsonFile('explorers.json');
+        const explorersByStack = ExplorerService.getExplorersByStack(explorers, "javascript");
+        expect(explorersByStack[0].stacks).toContain("javascript");
+        
     });
 
 });
