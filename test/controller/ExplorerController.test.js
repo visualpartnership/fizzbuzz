23fba5887ab45522ded9c3b463c11ac5ccc26b04
Luis <launchx04206@innovaccion.mx>
2022-05-05 18:49:28 -0500

    23. Creando prueba de unidad para la clase ExplorerController y validando si el método funciona

---
+++ test/controller/ExplorerController.test.js
@@ -0,0 +1,9 @@
+const ExplorerController = require ('./../../lib/controllers/ExplorerController')
+
+describe("Test para ExplorerController", () =>{
+    test('Filtrar explorers por su stack', () => { 
+        const stack = "javascript";
+        const explorersInJS = ExplorerController.filterByStack(stack);
+        expect(explorersInJS.length).toBe(11);
+     })
+})
