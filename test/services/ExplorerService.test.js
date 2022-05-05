ba416aa0ac7a4f061611302a2ca1a5ec08112d23
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-05-05 12:21:57 -0500

    Prueba requerimiento 4

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,21 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+    test("Requerimiento 4: Probando si filtra explorers por stack", () => {
+        const explorers = [
+            {
+                mission: "node",
+                nombre: "Explorer1",
+                stacks: ["java", "ruby", "typescript"]
+            },
+            {
+                mission: "java",
+                nombre: "Explorer2",
+                stacks: ["elixir", "javascript", "groovy"]
+            }
+        ];
+        const explorersStack = ExplorerService.filterByStack(explorers, "elixir");
 
+        expect(explorersStack.length).toEqual(1);
+    });
 });
