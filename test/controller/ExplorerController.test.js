85789cba4f108f600f7ccafb2ea9bd8ce9f70cdd
selene <seleneugs@gmail.com>
2022-05-05 02:37:31 -0500

    ejecutando linter

--- test/controller/ExplorerController.test.js
+++ test/controller/ExplorerController.test.js
@@ -2,7 +2,7 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Tests para ExplorerController", () => {
     test("Requerimiento 1: Encontrar Explorers por stack", () => {
-        const explorersByStacks = ExplorerController.getExplorerByStack("javascript")
+        const explorersByStacks = ExplorerController.getExplorerByStack("javascript");
         expect(explorersByStacks.length).toBe(11);
     });
 });
