143c9bd85ef6bca4affe685ee78adf4919f2fb28
selene <seleneugs@gmail.com>
2022-05-05 01:42:30 -0500

    Probando el Explorer controler

--- test/controller/ExplorerController.test.js
+++ test/controller/ExplorerController.test.js
@@ -2,8 +2,7 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Tests para ExplorerController", () => {
     test("Requerimiento 1: Encontrar Explorers por stack", () => {
-        const explorersByStack = ExplorerController.getExplorerByStack("javascript")
-        expect(explorersByStack.length).toBe(15);
+        const explorersByStacks = ExplorerController.getExplorerByStack("javascript")
+        expect(explorersByStacks.length).toBe(11);
     });
-
 });
