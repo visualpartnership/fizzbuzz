1ca1d16fc98e9929ddd3882e60483807bc809260
Sheepshenko <sheepshenko@gmail.com>
2022-05-06 11:36:23 -0500

    Se crea prueba para realizar el filtrado por stack de los explorers.

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,5 +7,9 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
-
+    test("Nuevo Requerimiento: Obtener todos los explorers con el stack solicitado.", () =>{
+        const explorers = [{stacks: ["javascript"]}];
+        const filterstack = ExplorerService.getExplorerByStack(explorers, "javascript");
+        expect(filterstack.length).toBe(1);
+    });
 });
