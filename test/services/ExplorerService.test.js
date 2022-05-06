1b738be4142180ef0e1d79fa9bbf5f278e7e3436
idanielmeza <idanielmezaledezma@gmail.com>
2022-05-05 20:01:15 -0500

    Se agrego prueba unitaria de filtrado por stack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,4 +1,5 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
@@ -6,5 +7,12 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+
+    test('Prueba filtrado por stack', ()=>{
+
+        const explorers = ExplorerController.getExplorersStack("javascript");
+        expect(explorers[0].stacks).toContain("javascript");
+
+    })
 
 });
