2410a6c7cb555b4bb998ddd9990b6bf356cd2ff2
AliciaGaona <aligaovela@gmail.com>
2022-05-07 17:10:04 -0500

    Se agrega prueba unitaria para metodo que filtrará los exporers por stack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,20 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
-    test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
+  test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+    
+    test("Test para probar nueva funcionalidad=> todos los explorers que tengan en stack el valor recibido en la url: javascript", () =>{
+        const explorers =[{name: "Woopa1", stacks: [ "javascript","reasonML","elm"]},{ name: "Woopa2",stacks: [ "javascript","groovy","elm" ]},
+                        {name: "Woopa3",stacks: ["elixir","groovy","reasonML" ]}];
+        const explorersStack = ExplorerService.filterByStack(explorers,"javascript");
+        expect(explorersStack).toBe(1);
+    });
 
 });
+
+
+  
