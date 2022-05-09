7b89b927c5e1245321bbe1bd2b7a85dccce07614
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-09 00:16:46 -0500

    Se modifica el test para el controller

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,4 +1,4 @@
-const ExplorerController = require("./../../lib/controllers/ExplorerController");
+const ExplorerController = require("../../lib/controllers/ExplorerController");
 describe("Prueba de unidad de para ExplorerController", () => {
     test("Obtener explorers por misión", () => {
         const explorers = ExplorerController.getExplorersByStack("javascript");
