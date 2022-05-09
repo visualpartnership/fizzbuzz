cf8d7c0dc6a8b6d0e08b04bf49687de30720895a
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-09 12:51:12 -0600

    Ejecutando ESlint para corregir el codigo

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,19 +1,17 @@
-const Reader = require('./../../lib/utils/reader');
-const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/reader");
+const ExplorerService = require("../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
-
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
-        const explorers = [{mission: "node"}];
+        const explorers = [{ mission: "node" }];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
 
-    test('Prueba obteniendo explorers que tienen un stack en específico', () => {
-        const explorers = Reader.readJsonFile('./test/data/ExplorersFileTest.json')
-        const explorersInStackk = ExplorerService.getExplorersByStack(explorers, 'elm')
+    test("Prueba obteniendo explorers que tienen un stack en específico", () => {
+        const explorers = Reader.readJsonFile("./test/data/ExplorersFileTest.json");
+        const explorersInStackk = ExplorerService.getExplorersByStack(explorers, "elm");
 
-        expect(explorersInStackk.length).toBe(12)
-    })
-
+        expect(explorersInStackk.length).toBe(12);
+    });
 });
