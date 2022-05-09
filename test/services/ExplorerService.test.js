b7d49763b907a1a8fe6652ed4f5d83c85cdb2c5d
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-09 10:50:05 -0600

    Añadiendo nuevo metodo vacio a ExplorerService que filtre explorers por stack y haciendo su prueba

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,19 @@
+const Reader = require('./../../lib/utils/reader');
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
+
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+
+    test('Prueba obteniendo explorers que tienen un stack en específico', () => {
+        const explorers = Reader.readJsonFile('./data/ExplorersFileTest.json')
+        const explorersInStack = ExplorerService.getExplorersByStack(explorers, 'elm')
+
+        expect(explorersInStack).toContain('elm')
+    })
 
 });
