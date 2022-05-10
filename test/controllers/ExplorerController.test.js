9d29a3c53dba3692e2af6c466b2772d5f0757091
Marco <macdev.gmail@gmail.com>
2022-05-09 22:00:46 -0500

    Se creó el código del método getExplorersByStack en el ExplorerController y se pasaron todas las pruebas

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -3,7 +3,7 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 describe("Tests para ExplorerController", () => {
     test("Requerimiento 1: Devolver todos los explorers que tengan un valor específico en su campo stacks solo recibiendo el parámetro del stack", () => {
         
-        const explorersJavascriptInStacks = ExplorerController.ExplorersByStack("javascript");
+        const explorersJavascriptInStacks = ExplorerController.getExplorersByStack("javascript");
         expect(explorersJavascriptInStacks[0].stacks).toMatchObject(["javascript", "reasonML", "elm"]);
     });
 });
