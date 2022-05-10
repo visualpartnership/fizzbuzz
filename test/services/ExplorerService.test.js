163e937a356231ed27bb08c9d11602390384f3d6
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-10 10:05:37 -0700

    ExplorerService + unit test

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -5,6 +5,10 @@ describe("Tests para ExplorerService", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    }),
+    test("Requerimiento 2: Retornar los explorers que tengan javascript en su stack", () => {
+        const explorers = [{stack: ["javascript"]}, {stack: ["javascript", "elm"]}, {stack: ["elm"]}];
+        const explorersInJS = ExplorerService.getExplorersUsernamesByStack(explorers, "javascript");
+        expect(explorersInJS.length).toBe(2);
     });
-
 });
