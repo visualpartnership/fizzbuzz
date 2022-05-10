f75cf43bc6b6a825574f27d8a420ad1137079772
Miriam <miriamlunacarreon133@gmail.com>
2022-05-10 15:25:32 -0500

    Test getExplorersByStack of ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,8 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Tests para ExplorerController", ()=>{
+    test("Obtener los explorers que tengan javascript en su stack",()=>{
+        const result = ExplorerController.getExplorersByStack("javascript");
+        expect(result.length).not.toBe(0);
+    })
+})
