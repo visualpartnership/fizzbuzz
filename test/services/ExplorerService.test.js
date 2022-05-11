efd35c02ef14bdd4e2d41136f5ce7290c4a67129
Daniel Ramirez <contact@dann.dev>
2022-05-11 15:55:56 -0500

    add Test: Test para mMetodo en explorer service para obtener explorers que incluyan una tecnologia en su stack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,4 +7,22 @@ describe("Tests para ExplorerService", () => {
         expect(explorersInNode.length).toBe(1);
     });
 
+    test("Requerimiento 2: Cantidad de explorers por mission",() =>{
+        const explorers = [{mission: "node"},{mission: "node"},{mission: "node"},{mission: "java"},{mission: "java"},{mission: "java"},{mission: "java"}];
+        const explorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(explorersByMission).toBe(3);
+    });
+     
+    test("Requerimiento 3 por Usernames",()=>{
+        const explorers = [{mission: "node", githubUsername: "Testjolonauta1"},{mission: "node", githubUsername: "Testjolonauta2"},{mission: "node", githubUsername: "Testjolonauta3"},{mission: "java", githubUsername: "Testjolonauta4"},{mission: "java", githubUsername: "Testjolonauta5"},{mission: "java", githubUsername: "Testjolonauta6"},{mission: "java", githubUsername: "Testjolonauta7"}];
+        const ExplorerAmount = ExplorerService.getExplorersUsernamesByMission(explorers,"node");  
+        expect(ExplorerAmount[0]).toBe("Testjolonauta1");
+    });
+
+    test ("Requerimiento 4 por stack",()=>{
+        const explorers = [{mission: "node", githubUsername: "Testjolonauta1", name: "Testjolonauta1", stacks:["javascript"]},{mission: "node", githubUsername: "Testjolonauta2", name: "Testjolonauta2", stacks:["javascript"]},{mission: "node", githubUsername: "Testjolonauta3", name: "Testjolonauta3", stacks:["typescript"]},{mission: "java", githubUsername: "Testjolonauta4", name: "Testjolonauta4" , stacks:["spring"]},{mission: "java", githubUsername: "Testjolonauta5", name: "Testjolonauta5", stacks:["spring"]},{mission: "java", githubUsername: "Testjolonauta6", name: "Testjolonauta6", stacks:["spring"]},{mission: "java", githubUsername: "Testjolonauta7", name: "Testjolonauta7", stacks:["spring"]}];
+        const explorerstack = ExplorerService.getExplorerWithStack(explorers, "javascript");
+        expect(explorerstack[0].name).toBe("Testjolonauta1");
+    });
+
 });
