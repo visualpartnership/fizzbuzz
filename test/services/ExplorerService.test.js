c261a1c70fcee7cac9e03cd0abb1834afc7ac708
GrisGGG <GrisGGG>
2022-05-10 20:22:07 -0500

    Paso 21: Se añadio el método static filterByStack en ExplorerService

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,5 +6,10 @@ describe("Tests para ExplorerService", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
     });
+    test("Requerimiento : Filtrar a los explorers por su stack", () =>{
+        const explorers = [{stacks: ["node", "javascript"]}, {stacks: ["node", "javascript"]}];
+        const explorersByStack = ExplorerService.filterByStack(explorers, "javascript")
+        expect(explorersByStack.length).toBe(2);
+    })
 
 });
