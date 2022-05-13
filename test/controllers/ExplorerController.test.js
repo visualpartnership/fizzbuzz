b7993277cd1caf1a60b8f95865e7b0888ce81913
CeViMu <vce02060@innovaccion.mx>
2022-05-12 22:17:25 -0500

    COorrigiendo estilos con linter

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -4,5 +4,5 @@ describe("Unit test", () =>{
     test("Tes para filtar a los explorers por stacks", ()=>{
         const explorersWithJavaScript = ExplorerController.getExplorersWithStack("javascript");
         expect(explorersWithJavaScript.length).toBe(11); 
-    })
-}) 
+    });
+}); 
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -9,10 +9,10 @@ describe("Tests para ExplorerService", () => {
 
     test("Requerimiento 2: Obtener los explorer que tengan en stack el valor recibido", () =>{
         const explorers = [{name: "Woopa1", stacks:["javascript", "reasonML", "elm"]},
-                           {name: "Woopa2", stacks:["javascript", "groovy", "elm"]},
-                           {name: "Woopa3", stacks:[ "elixir", "groovy", "reasonML"]}]
+            {name: "Woopa2", stacks:["javascript", "groovy", "elm"]},
+            {name: "Woopa3", stacks:[ "elixir", "groovy", "reasonML"]}];
         const explorersWithStack = ExplorerService.filterByStack(explorers, "javascript");
         expect(explorersWithStack.length).toBe(2);
-    })
+    });
 
 });
