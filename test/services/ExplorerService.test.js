d172f746be1e469c39ce21b802153511dbd9604a
D-CIoud <36202151+D-CIoud@users.noreply.github.com>
2022-05-12 21:08:50 -0500

    Agregado el endpoint para consultar explorers por stack, así mismo se genero su respectiva prueba de unidad y se agregaron las funcionalidades faltantes para que se ejecute correctamente

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,18 @@
-const ExplorerService = require("./../../lib/services/ExplorerService");
+const ExplorerService = require("../../lib/services/ExplorerService");
+const Reader = require("../../lib/utils/reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 2: Retornar Lista de Explorers que contengan en Stack el valor proporcionado", () => {
+        const explorersList = Reader.readJsonFile("explorers.json");
+        const stack = "javascript";
+        const explorersInStack = ExplorerService.getExplorersByStack(explorersList, stack);
+        expect(explorersInStack[0].stacks).toContain("javascript");
     });
 
 });
