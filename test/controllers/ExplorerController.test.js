aa2ff3a08fe405bf658fa165b6f242e868e7ebf2
CeViMu <vce02060@innovaccion.mx>
2022-05-12 21:37:00 -0500

    Agregando prueba fallida para el ExplorerController

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,8 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Unit test", () =>{
+    test("Tes para filtar a los explorers por stacks", ()=>{
+        const explorersWithJavaScript = ExplorerController.getExplorersWithStack("javascript");
+        expect(explorersWithJavaScript.length).toBe(11); 
+    })
+}) 
