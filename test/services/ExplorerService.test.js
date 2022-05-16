c631f9fcf3f27619e725c903e3108eb31b647ce6
danmtnezcas <danmtnezcas@outlook.com>
2022-05-16 02:08:45 -0500

    Modificando archivo ExplorerService.test.js, agregando prueba para método filterByStack

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,3 +1,4 @@
+const Reader = require("../../lib/utils/reader");
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
@@ -5,6 +6,18 @@ describe("Tests para ExplorerService", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+    test("Obtener todos los explorers en un stack", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInStack = ExplorerService.filterByStack(explorers, "javascript");
+        expect(explorersInStack).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({"name": "Woopa1"}), // This explorer is in javascript
+                expect.objectContaining({"name": "Woopa2"}), // This explorer is in javascript
+                //expect.objectContaining({"name": "Woopa3"}), // This explorer isn't in javascript
+                expect.objectContaining({"name": "Woopa15"}) // This explorer is in javascript
+            ])
+        );
     });
 
 });
