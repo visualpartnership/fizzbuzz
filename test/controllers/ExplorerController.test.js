d6ad94e9b1d45a13cabd4e4e199633711146bfa1
= <=>
2022-05-18 18:22:49 -0500

    Agregando dependencia Linter y corrigiendo errores

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,10 +1,9 @@
-const { TestWatcher } = require('jest');
-const ExplorerController = require('./../../../fizzbuzz/lib/controllers/ExplorerController');
+const ExplorerController = require("./../../../fizzbuzz/lib/controllers/ExplorerController");
 
-describe('Unit Test for ExplorerController',()=>{
-    test('Parte 1:Prueba de unidad para llamar la funcion filtrar por stack',()=>{
-        const ExplorersInNodeJavascript = ExplorerController.getExplorersByStack('javascript');
+describe("Unit Test for ExplorerController",()=>{
+    test("Parte 1:Prueba de unidad para llamar la funcion filtrar por stack",()=>{
+        const ExplorersInNodeJavascript = ExplorerController.getExplorersByStack("javascript");
 
         expect(ExplorersInNodeJavascript).not.toBeUndefined();
     });
-})
+});
