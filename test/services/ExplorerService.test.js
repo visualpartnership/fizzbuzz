c5d808d2889b642b1a32d6fb8e0024402c179869
= <=>
2022-05-18 18:03:37 -0500

    Agregando funcion GetExplorersByStack en la clase ExplorerController

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,7 +7,7 @@ describe("Tests para ExplorerService", () => {
         expect(explorersInNode.length).toBe(1);
     });
     test('Requerimiento 2: Mostrar todos los explorers por stack',()=>{
-        const explorers = [{name:'ajolote1',stack:['node','java']},{name:'ajolote2',stack:['node','java']},{name:'ajolote3',stack:'node'},{name:'ajolote4',stack:'java'}];
+        const explorers = [{name:'ajolote1',stacks:['node','java']},{name:'ajolote2',stacks:['node','java']},{name:'ajolote3',stacks:'node'},{name:'ajolote4',stacks:'java'}];
         const explorerInNode = ExplorerService.filterByStack(explorers,'node');
         expect(explorerInNode).not.toBeUndefined()
     })
