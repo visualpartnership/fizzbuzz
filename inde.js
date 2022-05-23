63b96702c042c20294ec88cede39dec656b6a9b9
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-23 15:15:19 -0500

    Agregando nuevo endpoint

--- inde.js
+++ inde.js
@@ -1,15 +1,15 @@
-const Reader = require('./lib/utils/reader')
-const ExplorerService = require('./lib/services/ExplorerService')
+const Reader = require("./lib/utils/reader");
+const ExplorerService = require("./lib/services/ExplorerService");
 
-const explorersReaders  = Reader.readJsonFile("explorers.json")
-console.log(explorersReaders)
+const explorersReaders  = Reader.readJsonFile("explorers.json");
+console.log(explorersReaders);
 
-const getExplorersWithStack = ExplorerService.getExplorersUsernamesByMission(explorersReaders, "node")
-console.log(getExplorersWithStack)
+const getExplorersWithStack = ExplorerService.getExplorersUsernamesByMission(explorersReaders, "node");
+console.log(getExplorersWithStack);
 // console.log(ExplorerService.getAmountOfExplorersByMission(explorersReaders, "node"));
 
-const filterExplorersByMission = ExplorerService.filterByMission(explorersReaders, "java")
-console.log(filterExplorersByMission)
+const filterExplorersByMission = ExplorerService.filterByMission(explorersReaders, "java");
+console.log(filterExplorersByMission);
 
 /*const getExplorersByStack = ExplorerService.filterByMission(explorersReaders, "node")
 console.log("imprimiendo la lista de explorers filtrandos por mision")
@@ -20,5 +20,5 @@ console.log(filterExplorersByMission) */
 console.log(getExplorersByStack)  */
 
 
-const getAmoung = ExplorerService.getAmountOfExplorersByMission(explorersReaders, "node")
-console.log(getAmoung)
+const getAmoung = ExplorerService.getAmountOfExplorersByMission(explorersReaders, "node");
+console.log(getAmoung);
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,7 +4,7 @@ describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular total de explorers por mision", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-        expect(explorersInNode).toBe(1)
+        expect(explorersInNode).toBe(1);
     });
     test("Endpoint Nuevo: Calcular la lista de explorers filtrados por un stack", () => {
         const explorers = [{mission: "node"}];
