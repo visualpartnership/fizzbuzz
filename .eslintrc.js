054f214fdf21f60b3d47b859cc8699bb7542215c
David Malpica <dmalpica96@hotmail.com>
2022-06-01 19:10:43 -0500

    Agregar metodo para filtrar explorers por stack

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,17 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true,
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest"
     },
     "rules": {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"]
     }
-}
+};
