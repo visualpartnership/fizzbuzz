30d9c64d2fb8e81625ccb17ebb14df34c1dca4cf
Axel Reyes <rbaxel.f@gmail.com>
2022-06-09 16:53:48 -0500

    agregando prueba y haciendola fallar

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,17 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+    test("Requerimiento 4: Filter explorers by Stack", () => {
+	const explorers=Reader.readJsonFile("explorers.json");
+        const explorersInStackE = ExplorerService.getExplorersByStack(explorers,"elixir");
+        const explorersAmountInStackE = explorersInStackE.length;
+        expect(explorersAmountInStackE.length).toBe(9);
     });
 
 });
