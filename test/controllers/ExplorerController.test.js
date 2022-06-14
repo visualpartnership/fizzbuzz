40635ba0bb12ccc4081d2dd3e0ff4c3ce7e10c9c
Luis Villegas <lualviva97@gmail.com>
2022-06-14 17:38:13 -0500

    Corregido el expect en la prueba para ExplorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,9 +1,7 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
-const ExplorerService = require("./../../lib/services/ExplorerService");
 describe("Test para ExplorerController", () => {
   test("1) Filtrar por stack", () => {
     const respuesta = ExplorerController.getExplorersByStack("javascript");
-    // expect(respuesta[1]).toMatch("javascript");
-    console.log(respuesta);
+    expect(respuesta[0]).toMatch("javascript");
   });
 });
