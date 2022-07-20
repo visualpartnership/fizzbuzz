7a971d0ce5197ec15b97b04c008e12c5ac0522a0
Angel <angelSistemas21@gmail.com>
2022-07-20 15:49:39 -0500

    test: Pruebas para el requerimiento 4 - getExplorersByStacks

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,3 +1,4 @@
+const Reader = require("../../lib/utils/reader");
 const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
@@ -5,6 +6,150 @@ describe("Tests para ExplorerService", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    });
+
+    test("Requerimiento 4: Calcular todos los explorers que tengan en stack un valor recibido", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+
+        const explorersByStacks = ExplorerService.getExplorersByStacks(explorers, "javascript");
+        expect(explorersByStacks).toEqual([
+            {
+                "name": "Woopa1",
+                "githubUsername": "ajolonauta1",
+                "score": 1,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa2",
+                "githubUsername": "ajolonauta2",
+                "score": 2,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "groovy",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa4",
+                "githubUsername": "ajolonauta4",
+                "mission": "node",
+                "score": 4,
+                "stacks": [
+                    "javascript"
+                ]
+            },
+            {
+                "name": "Woopa5",
+                "githubUsername": "ajolonauta5",
+                "score": 5,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa9",
+                "githubUsername": "ajolonauta9",
+                "score": 9,
+                "mission": "java",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa10",
+                "githubUsername": "ajolonauta10",
+                "score": 10,
+                "mission": "java",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa11",
+                "githubUsername": "ajolonauta11",
+                "score": 11,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa12",
+                "githubUsername": "ajolonauta12",
+                "score": 12,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa13",
+                "githubUsername": "ajolonauta13",
+                "score": 13,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa14",
+                "githubUsername": "ajolonauta14",
+                "score": 14,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            },
+            {
+                "name": "Woopa15",
+                "githubUsername": "ajolonauta15",
+                "score": 15,
+                "mission": "node",
+                "stacks": [
+                    "javascript",
+                    "elixir",
+                    "groovy",
+                    "reasonML",
+                    "elm"
+                ]
+            }
+        ]);
+
+        expect(explorersByStacks.length).toBe(11);
+
     });
 
 });
